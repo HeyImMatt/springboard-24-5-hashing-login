0b0ee878279e843e9c2da315cca97a78a0c9e33a
heyimmatt <mrmcfarland@gmail.com>
2020-08-21 11:34:49 -0700

    Add User columns

--- models.py
+++ models.py
@@ -16,3 +16,9 @@ class User(db.Model):
     """User Model"""
 
     __tablename__ = users
+
+    username = db.Column(db.Text(20), primary_key=True, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text(50), nullable=False, unique=True)
+    first_name = db.Column(db.Text(30), nullable=False)
+    last_name = db.Column(db.text(30), nullable=False)
