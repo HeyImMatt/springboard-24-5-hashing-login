135c12c0bba873086d2a37d311307842e3a87094
heyimmatt <mrmcfarland@gmail.com>
2020-08-21 11:57:46 -0700

    Add register form template, home route, and register route. Update models columns to create properly

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User
-# from forms import RegisterForm, LoginForm
+from forms import RegisterForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///hashing_exercise"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 app.config["SECRET_KEY"] = "abc123"
 
 connect_db(app)
 db.create_all()
 
 toolbar = DebugToolbarExtension(app)
+
+@app.route('/')
+def home_route():
+    """Home Route"""
+
+    return redirect('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_route():
+    """New user registration route"""
+
+    form = RegisterForm()
+
+    return render_template('register_form.html', form=form)
--- models.py
+++ models.py
@@ -15,10 +15,10 @@ def connect_db(app):
 class User(db.Model):
     """User Model"""
 
-    __tablename__ = users
+    __tablename__ = "users"
 
-    username = db.Column(db.Text(20), primary_key=True, nullable=False, unique=True)
+    username = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
-    email = db.Column(db.Text(50), nullable=False, unique=True)
-    first_name = db.Column(db.Text(30), nullable=False)
-    last_name = db.Column(db.text(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
